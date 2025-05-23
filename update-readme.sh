856ec77bdc4cd66b0e4bf1352e30ac856a9ac9e3
Mateusz <mwasylow@gmail.com>
2025-05-23 14:47:34 +0200

    updating readme

--- update-readme.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Skapa README med GitHub Stats (privata commits via token)
-cat <<EOF > README.md
-# Hardek00's GitHub Stats
-
-![GitHub stats](https://github-readme-stats.vercel.app/api?username=Hardek00&show_icons=true&count_private=true&include_all_commits=true&hide_title=true)
-
-EOF
